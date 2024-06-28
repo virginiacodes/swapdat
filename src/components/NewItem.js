1382afce09cae77216eedc4e60bd6e0a52aebb99
Virginie Helmreich <122171893+virginiacodes@users.noreply.github.com>
2024-06-28 10:04:15 +0100

    Update NewItem.js

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,4 +1,56 @@
-export default function NewItem({ isNewItemWindowOpen, onCreateNewItem }) {
+import { useState } from "react";
+
+export default function NewItem({
+  isNewItemWindowOpen,
+  onCreateNewItem,
+  newItem,
+  setNewItem,
+  items,
+  setItems,
+}) {
+  const [numberOfItems, setNumberOfItems] = useState(4);
+  const [userInput, setUserInput] = useState({
+    id: 0,
+    name: "",
+    image: null,
+    description: "",
+    postcode: "",
+    liked: false,
+    categorie: "",
+    free: true,
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUserInput({
+      ...userInput,
+      [name]: value,
+    });
+  }
+
+  function handleImageChange(e) {
+    setUserInput({
+      ...userInput,
+      image: URL.createObjectURL(e.target.files[0]),
+    });
+  }
+
+  function handleSubmit() {
+    const newItem = {
+      id: numberOfItems,
+      name: userInput.name,
+      image: userInput.image,
+      description: userInput.description,
+      postcode: "",
+      liked: false,
+      categorie: "",
+      free: true,
+    };
+    setItems([...items, newItem]);
+    setNumberOfItems(numberOfItems + 1);
+    onCreateNewItem();
+  }
+
   return (
     <div className="new-item">
       <div className="new-item-form">
@@ -9,8 +61,10 @@ export default function NewItem({ isNewItemWindowOpen, onCreateNewItem }) {
           <input
             type="text"
             id="title"
-            name="title"
+            name="name"
             placeholder="Title..."
+            value={userInput.name}
+            onChange={handleChange}
             required
           />
           <textarea
@@ -20,6 +74,8 @@ export default function NewItem({ isNewItemWindowOpen, onCreateNewItem }) {
             placeholder="Description..."
             maxLength="40"
             rows="2"
+            value={userInput.description}
+            onChange={handleChange}
             required
           />
           <input
@@ -29,6 +85,8 @@ export default function NewItem({ isNewItemWindowOpen, onCreateNewItem }) {
             placeholder="Postcode..."
             minLength="4"
             maxLength="8"
+            value={userInput.postcode}
+            onChange={handleChange}
             required
           />
           <select>
@@ -48,8 +106,13 @@ export default function NewItem({ isNewItemWindowOpen, onCreateNewItem }) {
             id="photo-upload"
             name="photo-upload"
             accept="image/png, image/jpeg"
+            onChange={handleImageChange}
           />
-          <button id="upload-button" type="submit">
+          <button
+            id="upload-button"
+            type="submit"
+            onClick={() => handleSubmit()}
+          >
             Upload item
           </button>
         </form>
